f900fcc45af22d2be91bbaf4e05c1a3ac5038e84
giancarlo.marcolin@gmail.com <giancarlo.marcolin@gmail.com>
2024-08-07 08:43:59 +0200

    Debug folder removed

--- TEST/testPrj/CM7/Debug/cppLibrary/utils/utils.d
+++
@@ -1,4 +0,0 @@
-cppLibrary/utils/utils.o: \
- /home/gian793/Documents/Projects/stm32h7-cppLibrary/utils/utils.cpp \
- /home/gian793/Documents/Projects/stm32h7-cppLibrary/utils/utils.h
-/home/gian793/Documents/Projects/stm32h7-cppLibrary/utils/utils.h:
